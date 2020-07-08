a1f4d4627f1bd46d916d49ed8ae6786161f3f428
Nebil Aydi <aydi.nebil@gmail.com>
2020-07-08 18:41:36 +0100

    Update manifest file ..

--- pdf_viewer/__manifest__.py
+++ pdf_viewer/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'PDF Viewer Widget.',
     'version': '12.0.1.0.0',
     'author': 'Nebil Aydi',
-    'website': 'https://github.com/aydi-nebil/web/tree/11.0/web_tree_no_open',
+    'website': 'https://github.com/aydi-nebil/web/tree/12.0/pdf_viewer',
     'license': 'AGPL-3',
     'application': False,
     'installable': True,
